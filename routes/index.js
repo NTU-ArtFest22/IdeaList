e3c0eb5b5b63ed9ff23f65184c92779a76676737
how2945ard <how2945ard@gmail.com>
2015-10-18 15:34:39 +0800

    include css

--- routes/index.js
+++ routes/index.js
@@ -21,23 +21,6 @@ module.exports = function(app) {
   app.use('/api', api);
   /*----- index page -----*/
 
-  app.get('/redirect', function(req, res) {
-    var msgType = req.query.msgType || '';
-    var messageAvaiable = {
-      '': '',
-      'loginRequired': 'You have to login first!'
-    };
-    var message = messageAvaiable[msgType];
-    var redirectUrl = req.query.redirectUrl;
-    var error = req.flash('error').toString();
-    return res.render('redirect',
-      passLocals(req, 'redirect', {
-        message: message,
-        error: error,
-        redirectUrl: redirectUrl
-      }));
-  });
-
   app.get('/', function(req, res) {
     return res.render('index', passLocals(req, 'index'));
   });
@@ -49,6 +32,6 @@ module.exports = function(app) {
 
   app.use(function(req, res) {
     req.flash('error', '404 Page Not Found');
-    return res.redirect('/redirect');
+    return res.redirect('/');
   });
 };
